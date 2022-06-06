9d2835c317651f68152f8bb3ccb01b453ba06f74
Behrad Ravanbod <99behi@gmail.com>
2022-06-06 18:07:05 +0430

    Add gorilla server example

--- cmd/websocket-server/main.go
+++ cmd/websocket-server/main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/websocket"
+)
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+var upgrader = websocket.Upgrader{}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		reverse(message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+func reverse(s []byte) {
+	var temp byte
+	for i := 0; i < len(s)/2; i++ {
+		temp = s[i]
+		s[i] = s[len(s)-i-1]
+		s[len(s)-i-1] = temp
+	}
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+	http.HandleFunc("/", echo)
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ravanbod/go-websocket-test
 
 go 1.18
+
+require github.com/gorilla/websocket v1.5.0 // indirect
